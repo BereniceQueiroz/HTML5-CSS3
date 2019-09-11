711f70bbf146ea406452a715a337e3af6d762bdf
Berenice Queiroz <52019177+BereniceQueiroz@users.noreply.github.com>
2019-09-11 13:57:57 -0300

    Add files via upload

---
+++ _javascript/funcoes.js
@@ -0,0 +1,7 @@
+	function mudaFoto (foto) {
+		
+		document.getElementById("icone").src=foto;
+
+	}
+
+	/*função utilizada para mudar a imagem ao mover o mouse sobre o menu, ir ao menu e criar a função para cada link com o endereço de cada foto a ser utilizada, buscando a função mudaFoto */
